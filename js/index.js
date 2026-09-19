ec9704b5ea601dfe309855a5e30e28fdb237d999
Paulo Henrique Da Sillva <paulllos38@gmail.com>
2026-09-19 13:08:03 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 const cards = document.getElementById("cards");
 
 let quantidadeProdutos = 10;
@@ -12,30 +13,30 @@ function mostrarProdutos(lista) {
 
         <div 
             onclick="window.location.href='produto.html?id=${produto.id}'"
-
-            class="bg-white rounded-2xl mt-4 md:mt-8 shadow-lg p-3 md:p-6 w-[85%] sm:w-[70%] md:w-full hover:scale-105 transition duration-300 cursor-pointer"
+            class="bg-white rounded-2xl mt-8 shadow-lg p-6 w-[60%] md:w-full hover:scale-105 transition duration-300 cursor-pointer"
         >
 
             <img 
-                class="w-full h-40 sm:h-48 md:h-64 object-cover rounded-xl"
+                class="w-full h-64 object-cover rounded-xl"
                 src="${produto.imagem}"
             >
 
-            <h2 class="text-xl md:text-2xl font-bold mt-3 md:mt-4">
+            <h2 class="text-2xl font-bold mt-4">
                 ${produto.preco}
             </h2>
 
-            <h2 class="text-lg md:text-2xl font-semibold">
+            <h2 class="text-2xl font-semibold">
                 ${produto.titulo}
             </h2>
 
-            <p class="text-sm md:text-base text-gray-500 mt-2">
+            <p class="text-gray-500 mt-2">
                 ${produto.descricao}
             </p>
 
         </div>
 
         `;
+
     });
 }
 
